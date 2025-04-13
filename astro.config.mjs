04ca9e7446b92a97bd1f3ff8d0333948995c75d5
Luke Bennett <hello@lukebennett.com.au>
2025-04-13 16:46:50 +1000

    Set up Netlify and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
 // @ts-check
 
 import markdoc from '@astrojs/markdoc';
+import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import keystatic from '@keystatic/astro';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	experimental: {
-		svg: true,
-	},
-	integrations: [react(), markdoc(), keystatic()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
+	adapter: netlify(),
+	build: { format: 'file' },
+	integrations: [keystatic(), markdoc(), react(), sitemap()],
+	output: 'static',
+	server: { host: true },
+	site: 'https://thatsferntastic.com.au',
+	trailingSlash: 'never',
+	vite: { plugins: [tailwindcss()] },
 });
